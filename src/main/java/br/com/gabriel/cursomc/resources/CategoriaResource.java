76dc3108fab40b3c674d49230969ace5f6eaece0
Gabriel <negraoga.silva@outlook.com>
2018-10-04 14:49:51 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/br/com/gabriel/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/gabriel/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
  package br.com.gabriel.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gabriel.cursomc.domain.Categoria;
 
 @RestController 
 @RequestMapping(value="/categorias")
@@ -12,8 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 //	No padrão Rest precisamos atribuir os verbos HTTP adquados para cada operação. 
 //	Como ela é uma funcao Rest 	eu preciso associar ela a um dos verbos do HTTP.
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {	
-		return "REST está funcionando!";
+	public List<Categoria> listar() {	
+		
+		Categoria cat1 = new Categoria(1, "Infomática");
+		Categoria cat2 = new Categoria(2, "Escritória");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 
