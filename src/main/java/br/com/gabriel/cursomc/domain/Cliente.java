f7255ebde69665d443c37dee789b3316f178058c
Gabriel Negrão Silva <gabriel.negraosilva@outlook.com>
2018-10-08 00:38:53 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/br/com/gabriel/cursomc/domain/Cliente.java
+++ src/main/java/br/com/gabriel/cursomc/domain/Cliente.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
@@ -42,6 +43,9 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 		super();
@@ -112,6 +116,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -136,8 +148,6 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-		
-	
+
 
 }
