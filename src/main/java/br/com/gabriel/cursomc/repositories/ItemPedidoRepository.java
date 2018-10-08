d453533c8bf0f51421e499266ee5fa113f8c450b
Gabriel <negraoga.silva@outlook.com>
2018-10-08 14:46:51 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/br/com/gabriel/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,16 @@
+package br.com.gabriel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.gabriel.cursomc.domain.ItemPedido;
+
+@Repository
+//JpaRepository é um tipo especial do Spring capaz de acessar os dados com base em um tipo que for passado.
+//Os parâmetros passados, são o tipo do objeto e o tipo do atributo identificador
+//Esse objeto realizará operações de acesso a dados (busca, salvar, alterar, deletar) todas referentes ao objeto ItemPedido que está
+//mapeado com a tabela ItemPedido no banco de dados.
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
