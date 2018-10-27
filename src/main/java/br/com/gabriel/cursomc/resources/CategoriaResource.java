1faa6c96c29d740e0463643b86f3e6e08ff66626
Gabriel Negrão Silva <gabriel.negraosilva@outlook.com>
2018-10-27 19:52:28 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/br/com/gabriel/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/gabriel/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,38 @@
- package br.com.gabriel.cursomc.resources;
+package br.com.gabriel.cursomc.resources;
 
- import br.com.gabriel.cursomc.domain.Categoria;
- import br.com.gabriel.cursomc.services.CategoriaService;
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.http.ResponseEntity;
- import org.springframework.web.bind.annotation.PathVariable;
- import org.springframework.web.bind.annotation.RequestMapping;
- import org.springframework.web.bind.annotation.RequestMethod;
- import org.springframework.web.bind.annotation.RestController;
+import br.com.gabriel.cursomc.domain.Categoria;
+import br.com.gabriel.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@RestController 
+import java.net.URI;
+
+@RestController
 @RequestMapping(value="/categorias")
 //Aqui criamos uma classe que vai ser o controlador Rest e vai responder pelo endpoint do RequestMapping
- public class CategoriaResource {
-	
-	@Autowired
-	private CategoriaService service;
+public class CategoriaResource {
 
-//	No padrão Rest precisamos atribuir os verbos HTTP adquados para cada operação. 
+    @Autowired
+    private CategoriaService service;
+
+    //	No padrão Rest precisamos atribuir os verbos HTTP adquados para cada operação.
 //	Como ela é uma funcao Rest 	eu preciso associar ela a um dos verbos do HTTP.
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {	
-		Categoria obj = service.buscar(id);
-		return ResponseEntity.ok().body(obj);
-								
-		
-	}
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    @RequestMapping(method =RequestMethod.POST )
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+        // Serve para retornar a nova uri que foi atribuida para a categoria inserida.
+        URI  uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
+
+    }
 
 }
