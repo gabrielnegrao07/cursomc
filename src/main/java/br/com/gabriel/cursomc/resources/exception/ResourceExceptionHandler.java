f7ef44ee56f14a7a08ff99c592f2763cef0cfdb3
Gabriel Negrão Silva <gabriel.negraosilva@outlook.com>
2018-10-27 21:10:32 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/br/com/gabriel/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/gabriel/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.gabriel.cursomc.resources.exception;
 
+import br.com.gabriel.cursomc.services.exceptions.DataIntegrityException;
 import br.com.gabriel.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +25,13 @@ public class ResourceExceptionHandler {
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
-		
-		
 	}
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
 
 }
